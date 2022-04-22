78bf831b977e5b28209f5f089c066e66944c6656
andreysaputra <sptrandrey@gmail.com>
2022-04-22 08:35:23 +0700

    Input Header

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -4,11 +4,12 @@ import propTypes from "prop-types";
 
 export default function Button(props) {
   const className = [props.className];
-  if (props.isprimary) className.push["btn-primary"];
-  if (props.isLarge) className.push["btn-lg"];
-  if (props.isSmall) className.push["btn-sm"];
-  if (props.isBlock) className.push["btn-block"];
-  if (props.hasShadow) className.push["btn-shadow"];
+  if (props.isPrimary) className.push("btn-primary");
+  if (props.isLight) className.push("btn-light");
+  if (props.isLarge) className.push("btn-lg");
+  if (props.isSmall) className.push("btn-sm");
+  if (props.isBlock) className.push("btn-block");
+  if (props.hasShadow) className.push("btn-shadow");
 
   const onClick = () => {
     if (props.onClick) props.onClick();
@@ -21,7 +22,7 @@ export default function Button(props) {
         {props.isLoading ? (
           <>
             <span className="spinner-border spinner-border-sm mx-5"></span>
-            <span className="sr-only">Loading ...</span>
+            <span className="sr-only">Loading...</span>
           </>
         ) : (
           props.children
@@ -74,9 +75,11 @@ Button.propTypes = {
   href: propTypes.string,
   target: propTypes.string,
   className: propTypes.string,
-  isLoading: propTypes.bool,
+  isPrimary: propTypes.bool,
+  isLight: propTypes.bool,
   isExternal: propTypes.bool,
   isDisabled: propTypes.bool,
+  isLoading: propTypes.bool,
   isSmall: propTypes.bool,
   isLarge: propTypes.bool,
   isBlock: propTypes.bool,
